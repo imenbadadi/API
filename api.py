e2401f11202514bc979be8cb4863ad370049ee28
imenbadadi <93078731+imenbadadi@users.noreply.github.com>
2022-11-28 06:58:52 +0100

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,7 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import key
+#import key
 
 
 url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
@@ -20,7 +20,8 @@ parameters = {
 }
 headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': key.api_key,
+    # 'X-CMC_PRO_API_KEY': key.api_key,
+    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
 }
 
 session = Session()
